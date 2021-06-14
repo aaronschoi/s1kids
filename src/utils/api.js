26d7e1eba8c12092e87595b581fb056efbe64b5b
AaronChoi <aaronschoilcw@gmail.com>
2021-06-14 15:01:07 -0700

    Redeploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL
 
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
